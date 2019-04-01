61a697cdd4cbee6c0416b4e1dff572ac54244524
shashankkumarIITB <shashankkumar.cse.iitb@gmail.com>
2019-04-01 20:37:05 +0530

    EmailPhase2 completed.

---
+++ SimpleEmailClientPhase2.cpp
@@ -0,0 +1,110 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <string.h> 
+#include <sys/socket.h>
+
+using namespace std;
+
+// Most of the comments have been provided in the SimpleEmailServerPhase1.cpp
+
+void NumArguements(const int numarg) {
+	if (numarg != 3) {
+		cout << "Usage : [filename] [ServerIPAddress]:[PortNumber] [Username] [Password]" << endl;	
+		exit(1);
+	}
+	return;
+}
+
+
+int CreateSocket() {
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+	perror("ERROR opening socket \nReason ");
+	exit(2);
+	}
+	return sockfd;
+}
+
+
+int ConnectSocket(int sockfd, sockaddr_in address) {
+	int connectfd = connect(sockfd, (struct sockaddr *) &address, sizeof(address));
+	if (connectfd < 0) {
+		perror("ERROR : Connect failed \nReason ");
+		exit(2);
+	}
+	else {
+		char ip[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(address.sin_addr), ip, INET_ADDRSTRLEN);
+		cout << "ConnectDone : " << ip << ":" << htons(address.sin_port) << endl;
+	}
+	return connectfd;
+}
+
+
+string ReceiveMessage(int sockfd) {
+	int bufferlen = 1024;
+	char* buffer = new char[bufferlen + 1];
+	int bytesRecv = recv(sockfd, buffer, bufferlen + 1, 0);
+	return string(buffer);
+	/*while (bytesRecv != 0 or bytesRecv != -1) {
+		msg += string(buffer);
+		bytesRecv = recv(sockfd, buffer, bufferlen + 1, 0);
+	}*/
+}
+
+
+void SendMessage(int sockfd, string msg) {// char* buffer, int bufferlen) {
+	int bufferlen = 1024;
+	int bytesToSend = msg.length();
+	int bytesSent = 0;
+	while (bytesToSend != 0) {
+		int currentBufferlen = min(bufferlen, bytesToSend);
+		char* buffer = new char[currentBufferlen + 1];
+		strcpy(buffer, msg.substr(bytesSent, currentBufferlen).c_str());
+		int sent = send(sockfd, buffer, currentBufferlen  + 1, 0);
+		bytesToSend = bytesToSend - sent + 1;
+		bytesSent = bytesSent + sent - 1;
+	}
+}
+
+
+int main(int argc, char* argv[]) {
+	int numarg = argc - 1;
+	string username = argv[2];
+	string password = argv[3];
+	NumArguements(numarg);
+
+	int clientfd = CreateSocket();
+
+	// Parse arguement to get serverIP and port number
+	char* serverIP = strtok(argv[1],":");
+	int portNum = atoi(strtok(NULL,":"));
+
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(portNum);
+	serverAddr.sin_addr.s_addr = inet_addr(serverIP);
+
+	// Connect socket to the server
+	int connectfd = ConnectSocket(clientfd, serverAddr);
+	
+	// Send login message
+	string login = "User: " + username + " Pass: " + password;
+	SendMessage(clientfd, login);
+
+	// Receive reply from server
+	cout << "Server : " << ReceiveMessage(clientfd) << endl;
+	SendMessage(clientfd, "quit");
+
+	/*
+	Exit status :
+	1 => Wrong number of arguements
+	2 => 
+		a) Socket creation failed
+		b) Bind failed
+	*/
+
+}
+
+
---
+++ SimpleEmailServerPhase2.cpp
@@ -0,0 +1,331 @@
+#include <arpa/inet.h>
+#include <cmath>
+#include <dirent.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <netinet/in.h>
+#include <sstream>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+using namespace std;
+
+
+void NumArguements(const int numarg) {
+	if (numarg != 3) {
+		cout << "Usage : [filename] [PortNumber] [Passwordfile] [FolderPath]" << endl;	
+		exit(1);
+	}
+	return;
+}
+
+
+void FileExists(const string& filename, map<string, string>& userdata) {
+  ifstream ifile(filename.c_str());
+  if (not (bool)ifile) {
+  	cout << "File : " << filename << " Does not exist." << endl;
+  	exit(3);
+  }
+  else if (ifile.good() == 0) {
+  	cout << "File : " << filename << " Read permission denied." << endl;
+  	exit(3);
+  }
+  else {
+  	string line;
+	while (getline(ifile, line))
+	{
+	    istringstream isstring(line);
+	    string username, password;
+	    if (!(isstring >> username >> password)) { 
+	    	continue;
+	    }
+	    userdata.insert({username, password});
+	}
+  }
+  return;
+}
+
+
+bool FolderExists (string folderpath) {
+	struct stat info;
+	char* buffer = new char[folderpath.length() + 1];
+	strcpy(buffer, folderpath.c_str());
+
+	if (stat(buffer, &info) != 0) {
+	// Check if the directory exists
+		return false;
+	}
+	else if (info.st_mode & S_IFDIR) {
+	// Check if the directory is accessible
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+int NumFiles (string path) {
+	// Returns the number of files and folders present at the specifies path 
+	string s = ".";
+	string s1 = "..";
+	char *ptr = new char[2];
+	char *ptr1 = new char[3];
+	char *buffer = new char[path.length() + 1];
+	strcpy(ptr, s.c_str());
+	strcpy(ptr1, s1.c_str());
+	strcpy(buffer, path.c_str());
+	
+	int count = 0;
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(buffer);
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			if (strcmp(ptr, dir->d_name) && strcmp(ptr1, dir->d_name)) {
+				// Uncomment the below line to display file/directory names
+				// cout << dir->d_name << endl;
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+
+int CreateSocket() {
+	// Create a socket with TCP protocol
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		perror("ERROR opening socket \nReason ");
+		exit(2);
+	}
+	return sockfd;
+}
+
+
+void DeleteSocket(int sockfd) {
+	/*int isetoption = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&isetoption, sizeof(isetoption));*/
+	shutdown(sockfd, 2);
+}
+
+
+int BindSocket(int sockfd, const sockaddr_in address) {
+	// Bind the socket with the address
+	int bindfd = bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+	if (bindfd < 0) {
+		perror("ERROR : Bind failed \nReason ");
+		DeleteSocket(sockfd);
+		exit(2);
+	}
+	else {
+		cout << "BindDone : " << htons(address.sin_port) << endl;
+	}
+	return bindfd;
+}
+
+
+int ListenSocket(int sockfd, int portNum) {
+	// Specify the length of queue
+	int queuelen = 5;
+
+	// Listen on the socket for the queue length specified
+	int listenfd = listen(sockfd, queuelen);
+	if (listenfd < 0) {
+		perror("ERROR : Listen failed \nReason ");
+		DeleteSocket(sockfd);
+		exit(2);
+	}
+	else {
+		cout << "ListenDone : " << portNum << endl;
+	}
+	return listenfd;
+}
+
+
+int AcceptConnection(int sockfd, const sockaddr_in address) {
+	socklen_t addrlen = sizeof(address);
+	int acceptfd = accept(sockfd, (struct sockaddr *)&address, &(addrlen));
+	if (accept < 0) {
+		perror("ERROR : Accept failed \nReason ");
+		DeleteSocket(sockfd);
+		exit(2);
+	}
+	else {		
+		char ip[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(address.sin_addr), ip, INET_ADDRSTRLEN);
+		cout << "Client : " << ip << " : " << address.sin_port << endl;
+	}
+	return acceptfd;
+}
+
+
+string ReceiveMessage(int sockfd) {
+	int bufferlen = 1024;
+	char* buffer = new char[bufferlen + 1];
+	int bytesRecv = recv(sockfd, buffer, bufferlen + 1, 0);
+	return string(buffer);
+}
+
+
+void SendMessage(int sockfd, string msg) {
+	int bufferlen = 1024;
+	int bytesToSend = msg.length();
+	int bytesSent = 0;
+	while (bytesToSend != 0) {
+		int currentBufferlen = min(bufferlen, bytesToSend);
+		char* buffer = new char[currentBufferlen + 1];
+		strcpy(buffer, msg.substr(bytesSent, currentBufferlen).c_str());
+		int sent = send(sockfd, buffer, currentBufferlen  + 1, 0);
+		bytesToSend = bytesToSend - sent + 1;
+		bytesSent = bytesSent + sent - 1;
+	}
+}
+
+
+void Validate(int serverfd, int sockfd, string s, map<string, string>& userdata, string& user) {
+	string User, Pass;
+	string username;
+	string password;
+	
+	istringstream isstring(s);
+	if (!(isstring >> User >> username >> Pass >> password)) {
+		if (User != "User:" or Pass != "Pass:") {
+		cout << "Unknown command." << endl;
+		DeleteSocket(sockfd);
+		exit(100);
+		}
+	}
+	else {
+	// Check if the user and password specifies belongs to the database 
+		bool userfound = false;
+		for (map<string, string>::iterator it = userdata.begin(); it != userdata.end(); it++) {
+			if (it->first == username) {
+				if (it->second == password) {
+					user = username; 
+					userfound = true;
+					string welcome = "Welcome, " + username;
+					cout << welcome << endl;
+					SendMessage(sockfd, welcome);
+				}
+				else {
+					cout << "Password incorrect." << endl;
+					DeleteSocket(sockfd);
+					exit(100);	
+				}
+			}
+			else {
+				continue;
+			}
+		}
+		if (!userfound){
+			cout << "Invalid User : " << username << endl;
+			DeleteSocket(sockfd);
+			exit(100);
+		}
+	}
+}
+
+
+int main(int argc, char*argv[]) {
+	int numarg = argc - 1;
+	
+	NumArguements(numarg);
+	string filename = argv[2];
+	string folderpath = argv[3];
+	map<string, string> userdata;
+	
+	FileExists(filename,userdata);
+	bool dirExists = FolderExists(folderpath);
+	
+	// Create a file descripter
+	int serverfd = CreateSocket();
+	int portNum = atoi(argv[1]);
+
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(portNum);
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+	BindSocket(serverfd, serverAddr);
+	ListenSocket(serverfd, portNum);
+<<<<<<< HEAD
+
+	struct sockaddr_in clientAddr;
+	int acceptfd = AcceptConnection(serverfd, clientAddr);
+
+	string recvmsg = ReceiveMessage(acceptfd);
+	
+	string username;
+	Validate(acceptfd, recvmsg, userdata, username);
+	
+	string recvmsg = ReceiveMessage(acceptfd);
+	if (recvmsg == "quit") {
+		cout << "Bye, " << username << endl;
+		DeleteSocket(acceptfd);
+		exit(0);
+	}
+	else {
+		cout << "Unknown command." << endl;
+		exit(100);
+=======
+	
+	while (true) {
+		struct sockaddr_in clientAddr;
+		int acceptfd = AcceptConnection(serverfd, clientAddr);
+
+		string recvmsg = ReceiveMessage(acceptfd);
+		string username;
+		Validate(serverfd, acceptfd, recvmsg, userdata, username);
+		
+		while (true) {
+			recvmsg = ReceiveMessage(acceptfd);
+			if (recvmsg == "quit") {
+				string msg = "Bye, " + username; 
+				cout << msg << endl;
+				SendMessage(acceptfd, "Bye.");
+				break;
+			}
+			else if (recvmsg == "LIST") {
+				if (FolderExists(folderpath + username + "/")) {
+				int n = NumFiles(folderpath + username + "/");
+				cout << username << " : Number of files : " << n << endl;
+				SendMessage(acceptfd, "Number of files : " + to_string(n));
+				}
+				else {
+					cout << username << " : Folder read failed." << endl;
+					SendMessage(acceptfd, "Folder read failed.");
+					break;
+				}
+			}
+			else {
+				cout << "Unknown command." << endl;
+				SendMessage(acceptfd, "Unknown command.");
+				break;
+			}
+		}
+>>>>>>> 8ae3dd0... EmailPhase2 Completed.
+	}
+
+		/*
+	Exit status :
+	0 => Success
+	1 => Wrong number of arguements
+	2 => 
+		a) Socket creation failed
+		b) Bind failed
+		c) Listen failed
+		d) Accept failed		
+	3 => Password file not present
+	100 => Otherwise
+	*/
+}
+
+
+
+
+
